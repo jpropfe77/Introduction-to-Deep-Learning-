770b0f464cada0562b95ee46d7e2f0aafc7d1580
jpropfe77 <jpropfe@gmail.com>
2026-08-27 10:50:34 -0600

    Add Assignment 1

---
+++ Assignment 1/setup_script.py
@@ -0,0 +1,13 @@
+# %%
+import platform
+
+print(f"I am running on Google's servers: {platform.node()}")
+
+# %%
+# Local Test Success.
+name = input("Enter your name: ")
+
+print(f"Happy coding, {name}!")
+
+
+
